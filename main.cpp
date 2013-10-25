745163220a230c5c56d6fe0aabcc59e155646c56
Naoki Ueda <orange.s067803@gmail.com>
2013-10-25 19:52:52 +0900

    規則正しく振動しすぎている。何かおかしい。

--- main.cpp
+++ main.cpp
@@ -33,27 +33,25 @@ int main()
     Agent agent[ NUM_A ];                       /* エージェントは複数  */
 
     Virus virus[ NUM_V ] = {                    /* ウイルス生成  */
-        /* 123456789012345 */
-        "110111011",
-        "100011100"
+      /* 123456789012345 */
+        "11011",
+        "10001"
     };
 
     Landscape *landscape = new Landscape;       /* ランドスケープ初期化 */
 
     Administrator AD( agent, virus, landscape ); /* 管理者に登録 */
 
-    int infected_all = 0;                         /* 免疫獲得者カウンタ  */
+    int infected_all = 0;                       /* 免疫獲得者カウンタ  */
 
-    ofstream ofs("A_hasImmunity.dat");          /* 出力ファイル  */
+    ofstream ofs("A_hasVirus.dat");             /* 出力ファイル  */
     ofstream ofs_log("A_log.dat");              /* 出力ファイル  */
 
     // Log 
     virus[0].printTag();
     virus[1].printTag();
-    agent[0].printTag();
-    agent[1].printTag();
 
-    for(int i=0; i<10; i++) {
+    for(int i=0; i<10; i++) {                   /* 感染させる */
         agent[ i ].infection( virus[0] );
     }
     for(int i=10; i<20; i++) {
@@ -74,7 +72,7 @@ int main()
         AD.responseAgent();                     /* 免疫応答（タグフリップ） */
 
         // CALCULATE 
-        infected_all = 0;                         /* 全ウイルスへの免疫獲得者数 */
+        infected_all = 0;                       /* 全ウイルスへの免疫獲得者数 */
         FOR( j, NUM_A )                         /* 免疫獲得者を計算  */
         {
             if( agent[j].isInfected( virus[0] ) &&
@@ -85,19 +83,7 @@ int main()
         ofs << i << SEPARATOR                   /* ファイルに出力  */
             << AD.numIsInfected( virus[0] ) << SEPARATOR            /* 免疫獲得者数  */
             << AD.numIsInfected( virus[1] ) << SEPARATOR            /* 免疫獲得者数  */
-            << infected_all << endl;              /* 全免疫獲得者数  */
+            << infected_all << endl;            /* 全免疫獲得者数  */
     }
-
-    // --------------- 
-    // log 
-    FOR(i, NUM_A) {
-        FOR(j, agent[0].len_) ofs_log<<agent[i].tag_[j];
-        ofs_log<<" "<<agent[i].numHoldingVirus();
-        ofs_log<<" "<<agent[i].hasImmunity(virus[0]);
-        ofs_log<<" "<<agent[i].hasImmunity(virus[1]);
-        ofs_log<<endl;
-    }
-    // --------------- 
-
     return 0;
 }
