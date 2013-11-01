ce8b764c8edb3e7fd09664d675380f743e7c1ce0
Naoki Ueda <orange.s067803@gmail.com>
2013-11-01 13:38:26 +0900

    ---

--- Global.h
+++ Global.h
@@ -20,17 +20,17 @@
 #define REP(i, min, max)    for(int (i)=(min); (i)<=(max); (i)++)
 #define SEPARATOR   " "
 
-typedef int tag_t   ; 
+typedef int tag_t; 
 
-const int WIDTH     = 120   ;                   /* ランドスケープの幅 */
+const int WIDTH             = 120   ; /* ランドスケープの幅 */
 
-const int NUM_V     = 2     ;                   /* ウイルスの種類  */
-const int NUM_A     = 10000 ;                   /* エージェントの数  */
+const int NUM_V             = 2     ; /* ウイルスの種類  */
+const int NUM_A             = 10000 ; /* エージェントの数  */
 
-const int TAG_LEN_A = 20    ; 
-const int TAG_LEN_V = 15     ;                   /* ウイルスのタグの長さ */
+const int TAG_LEN_A         = 20    ; 
+const int TAG_LEN_V         = 15    ; /* ウイルスのタグの長さ */
 
-const double INFECTION_RATE = 0.5     ;                   /* ウイルスのタグの長さ */
+const double INFECTION_RATE = 0.5   ; /* デフォルトの感染率 */
 
 template < typename T >                         /* デバッグ用 */
 void log(T str) { int static i=0;std::cout<<i++<<":"<<str<<std::endl; }
