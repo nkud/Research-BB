3d78fb3de6fdf5f0063063a78d87a1ffbe51089e
Naoki Ueda <orange.s067803@gmail.com>
2013-11-22 15:57:32 +0900

    ---

--- include/Global.h
+++ include/Global.h
@@ -31,28 +31,29 @@
 typedef int tag_t;                                         /* タグの型（通常 int ） */
 
 // Configure ==============================================
-#define NO_DIAGONAL         0                              /* 対角線４マスを近隣に含まない */
+#define NO_DIAGONAL         1                              /* 対角線４マスを近隣に含まない */
+#define RANDOM_LOCATE       0
 
 const int TERM                          = 300                   ; /* 期間 */
 const int OUTPUT_INTERVAL               = 1                     ; /* 出力する間隔 */
 
 /* landscape */
-const int WIDTH                         = 50                    ; /* ランドスケープの幅 */
+const int WIDTH                         = 15                    ; /* ランドスケープの幅 */
 
-const int NUM_V                         = 10                     ; /* ウイルスの種類 */
-const int NUM_A                         = 1000                  ; /* エージェントの数 */
+const int NUM_V                         = 2                     ; /* ウイルスの種類 */
+const int NUM_A                         = 100                    ; /* エージェントの数 */
 
-const double LEN_SCALE                  = 2.5                  ; /* ウイルスに対するエージェントのタグスケール */
+const double LEN_SCALE                  = 2.0                  ; /* ウイルスに対するエージェントのタグスケール */
 const int TAG_LEN_V                     = 10                    ; /* ウイルスのタグのデフォルトの長さ */
 const int TAG_LEN_A                     = LEN_SCALE * TAG_LEN_V ; /* エージェントのタグ長 */
 
 /* rate */
-const double INFECTION_RATE             = 0.5                   ; /* デフォルトの感染率 */
+const double INFECTION_RATE             = 1.0                   ; /* デフォルトの感染率 */
 const double INIT_INFECTED_RATIO        = 0.1                   ; /* 初期感染率 */
 
 /* max */
 const int MAX_VIRUS_AGENT_HAVE          = NUM_V                 ; /* 最大保持ウイルス */
-const int MAX_V_AGENT_INFECT_ONT_TIME   = 10                     ; /* １期間で感染させるウイルスの最大数 */
+const int MAX_V_AGENT_INFECT_ONT_TIME   = 1                     ; /* １期間で感染させるウイルスの最大数 */
 //const int MAX_STAND_BY_VIRUS_AGENT_HAVE = NUM_V - 1             ; /* 最大待機ウイルス */
 
 const int MOVE_DISTANCE                 = 1                     ; /* エージェントの移動距離 */
