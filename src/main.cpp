1c68c77bc685e434ac0a8e792b8496bd0adb44a0
Naoki Ueda <orange.s067803@gmail.com>
2013-11-25 11:38:30 +0900

    少し修正
    あと、交配と老化の関数を作る必要がある

--- src/main.cpp
+++ src/main.cpp
@@ -27,40 +27,39 @@ using namespace std;
 
 int main()
 {
-    Benchmark::Instance().startTimer();
+    Benchmark::Instance().startTimer();                    /* ベンチマーク計測開始 */
     srand( (unsigned int)time(NULL)/2 );                   /* 乱数初期化  */
 
     // 初期化
-    VECTOR(Agent) agent;                              /* エージェントの集合  */
-    Agent a[MAX_NUM_A];
-    FOR( i, MAX_NUM_A ) {
-        agent.push_back( a[i] );
+    VECTOR(Agent) agent;                                   /* エージェントの集合  */
+    FOR( i, MAX_NUM_A ) {                                  /* XXX 最大エージェントの数だけ */
+        agent.push_back( *(new Agent) );                   /* エージェントを初期化 */
     }
     Virus virus[ NUM_V ];                                  /* ウイルス生成 */
-    Landscape landscape;                  /* ランドスケープ初期化 */
+    Landscape landscape;                                   /* ランドスケープ初期化 */
 
-    // 管理者に登録
-    Administrator admin( agent, virus, &landscape );
+    Administrator admin( agent, virus, &landscape );       /* 管理者に登録 */
 
     Monitor &monitor = Monitor::Instance();                /* モニター */
 
     // 初期感染
     FOR( i, NUM_V ) {
-        admin.initInfectAgentInRatio( virus[i], INIT_INFECTED_RATIO );            /* 感染させる */
+        admin.initInfectAgentInRatio( virus[i], INIT_INFECTED_RATIO );            /* 初期感染させる */
     }
+
+    admin.relocateAgent();                                 /* ランダムに配置 */
 
     monitor.generatePlotScript();                          /* XXX: gnuplot用 */
 
     int zero_count = 0;
 
-    admin.relocateAgent();                                 /* ランダムに再配置 */
     /* 計測開始 */
     FOR( i, TERM )                                         /* 計算開始  */
     {
         log("------------ start");
         admin.incrementTerm();                             /* 期間を進める */
 
-        monitor.resetAll();                                /* カウンターをリセット */
+        monitor.resetAll();                                /* モニターのカウンターをリセット */
 
         admin.moveAgent();                                 /* 移動する */
         admin.contactAgent();                              /* 近隣に接触する */
@@ -74,10 +73,8 @@ int main()
 
         /* 途中経過 */
         log( monitor.getContactNum() );
-        log( agent[0].getX() );
-        log( agent[0].getY() );
-        if( monitor.getContactNum()==0 ) zero_count++;
-        if( zero_count >= 1 ) break;
+        if( monitor.getContactNum()==0 ) zero_count++;     /* １０回以上接触感染がなければ */
+        if( zero_count >= 10 ) break;                      /* 強制的に終了する */
     }
     
     // 確認用 -----------------------------------------------------------------
@@ -85,11 +82,12 @@ int main()
 
     // エージェントの最終的な状態など -----------------------------------------
     admin.outputFile_LastLog( "A_log.txt" );
-    // ------------------------------------------------------------------------
 
+    // ------------------------------------------------------------------------
+    // メモ
     // 計測時間出力                             /* XXX: ??? */
-    Benchmark::Instance().stopTimer();
-    Benchmark::Instance().printTime();
+    Benchmark::Instance().stopTimer();                     /* ベンチマークの計測終了 */
+    Benchmark::Instance().printTime();                     /* 計測時間表示 */
     log(sizeof(Agent));
     log(sizeof(Virus));
     log(sizeof(admin));
