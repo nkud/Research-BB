52774101d2dd3eb765d01d637312fa1c96e9ea5e
Naoki Ueda <orange.s067803@gmail.com>
2013-12-06 17:34:56 +0900

    ---

--- include/Global.h
+++ include/Global.h
@@ -52,7 +52,7 @@ typedef int tag_t;                                                   /* タグ�
 //#define COUPLE_TAG                                                   /* カップルタグで子供を作る */
 
 /* 期間 */
-const int TERM                        = 100000                   ;      /* 期間 */
+const int TERM                        = 50000                   ;   /* 期間 */
 const int OUTPUT_INTERVAL             = 1                     ;      /* 出力する間隔 */
 const int MINI_SIZE_TERM              = 200;
 
@@ -60,21 +60,21 @@ const int MINI_SIZE_TERM              = 200;
 const int WIDTH                       = 10                    ;      /* ランドスケープの幅 */
 
 /* ウイルス */                                                          /* ウイルスの設定 -------------------- */
-const int TAG_LEN_V                   = 8                    ;      /* ウイルスのタグのデフォルトの長さ */
+const int TAG_LEN_V                   = 8                    ;       /* ウイルスのタグのデフォルトの長さ */
 const int NUM_V                       = 2                      ;     /* ウイルスの種類 */
 const double INFECTION_RATE           = 1.0                  ;       /* デフォルトの感染率 */
 
 const int MAX_VIRUS_AGENT_HAVE        = NUM_V                 ;      /* 最大保持ウイルス */
-const int MAX_V_AGENT_INFECT_ONT_TIME = 1                 ;      /* １期間で感染させるウイルスの最大数 */
+const int MAX_V_AGENT_INFECT_ONT_TIME = 1                 ;          /* １期間で感染させるウイルスの最大数 */
 
 /* エージェント */                                                          /* エージェントの設定 ---------------- */
 const int TAG_LEN_A                   = 10                    ;      /* エージェントのタグ長 */
-const int INIT_NUM_A                  = 100                  ;       /* 初期エージェントの数 */
-const double INIT_INFECTED_RATIO      = 1.0                   ;      /* 初期感染率 */
+const int INIT_NUM_A                  = 1000                  ;       /* 初期エージェントの数 */
+const double INIT_INFECTED_RATIO      = 0.5                   ;      /* 初期感染率 */
 //const double LEN_SCALE                = 1.0                   ;      /* ウイルスに対するエージェントのタグスケール */
 //const int TAG_LEN_A                   = LEN_SCALE * TAG_LEN_V ;      /* エージェントのタグ長 */
 
-const int MAX_NUM_A                   = 30000                 ;        /* 最大エージェント数 */
+const int MAX_NUM_A                   = 30000                 ;      /* 最大エージェント数 */
 
 /* 移動 config */                                                    /* 移動専用の設定 -------------------- */
 const int MOVE_DISTANCE               = 1                     ;      /* エージェントの移動距離 */
