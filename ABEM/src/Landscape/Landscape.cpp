5f9e5f6f46b425d0cfba4768c7254800d30ca1ff
Naoki Ueda <orange.s067803@gmail.com>
2014-06-19 15:59:17 +0900

    Landscape修正

--- ABEM/src/Landscape/Landscape.cpp
+++ ABEM/src/Landscape/Landscape.cpp
@@ -17,40 +17,41 @@
 
 #include <cassert>
 
-// エージェントの近隣を返す
+
+/*
+ *--------------------------------------------------------------------------------------
+ *      Method:  Landscape :: getNeighbors
+ * Description:  近隣の配列を取得する
+ *--------------------------------------------------------------------------------------
+ */
 VECTOR(Agent *)
 Landscape :: getNeighbors( const Agent &agent )
 {
-    int x = agent.getX();
-    int y = agent.getY();
-    VECTOR(Agent *) neighbors;
-    REP(i, -1, 1)
-    {
-        REP(j, -1, 1)
-        {
-            if( isOnMap(x+i, y+j) )
-            {
-                EACH( it_a, getAgentList(x+i, y+j) ) {
-                    neighbors.push_back( *it_a );
-                }
-            }
+  int x = agent.getX();
+  int y = agent.getY();
+  VECTOR(Agent *) neighbors;                                         /* 近隣の配列 */
+  REP(i, -1, 1) {                                                    /* 周りの土地に対して */
+    REP(j, -1, 1) {
+      if( isOnMap(x+i, y+j) ) {                                      /* はみ出していなければ */
+        EACH( it_a, getAgentList(x+i, y+j) ) {                       /* その場所のエージェントを */
+          neighbors.push_back( *it_a );                              /* 近隣に追加する */
         }
+      }
     }
-    EACH( it_n, neighbors ) {
-        if ( *it_n == &agent )
-        {
-            neighbors.erase(it_n);
-            break;
-        }
+  }
+  EACH( it_n, neighbors ) {
+    if ( *it_n == &agent ) {                                         /* 自分は */
+      neighbors.erase(it_n);                                         /* 削除 */
+      break;
     }
-    return neighbors;
+  }
+  return neighbors;
 }
- 
+
 Landscape :: Landscape()
 {
-    // FOR( i, WIDTH ) FOR( j, WIDTH ) agent_map_[i][j].clear();        /* エージェントのマップをクリア */
-    clearAgentMap();
-    log("init Landscape");
+  clearAgentMap();
+  log("init Landscape");
 }
 
 
@@ -62,10 +63,10 @@ Landscape :: Landscape()
  *--------------------------------------------------------------------------------------
  */
 void Landscape :: putBackOnMap( int &x, int &y) {
-    while( x < 0 ) x += WIDTH;
-    while( y < 0 ) y += WIDTH;
-    x %= WIDTH;
-    y %= WIDTH;
+  while( x < 0 ) x += WIDTH;
+  while( y < 0 ) y += WIDTH;
+  x %= WIDTH;
+  y %= WIDTH;
 }
 /*
  *--------------------------------------------------------------------------------------
@@ -75,15 +76,15 @@ void Landscape :: putBackOnMap( int &x, int &y) {
  *--------------------------------------------------------------------------------------
  */
 void Landscape :: putAgentOnMap( Agent &a ) {
-    int x = a.getX();                                                /* 座標を取得する */
-    int y = a.getY();
+  int x = a.getX();                                                  /* 座標を取得する */
+  int y = a.getY();
 
-    putBackOnMap( x, y );
+  putBackOnMap( x, y );                                              /* 土地に配置する */
 
-    assert( isOnMap(x,y) );
+  assert( isOnMap(x,y) );
 
-    a.setX( x );                                                     /* 座標を設定しなおす */
-    a.setY( y );
+  a.setX( x );                                                       /* 座標を設定しなおす */
+  a.setY( y );
 }
 
 /*
@@ -94,11 +95,11 @@ void Landscape :: putAgentOnMap( Agent &a ) {
  */
 bool Landscape :: isOnMap( const int x, const int y) const
 {
-    if( x < 0 ) { return false; }
-    if( y < 0 ) { return false; }
-    if( x > WIDTH-1 ) { return false; }
-    if( y > WIDTH-1 ) { return false; }
-    return true;
+  if( x < 0 ) { return false; }
+  if( y < 0 ) { return false; }
+  if( x > WIDTH-1 ) { return false; }
+  if( y > WIDTH-1 ) { return false; }
+  return true;
 }
 
 /*
@@ -108,11 +109,11 @@ bool Landscape :: isOnMap( const int x, const int y) const
  *--------------------------------------------------------------------------------------
  */
 void Landscape :: clearAgentMap() {
-    FOR( i, WIDTH ) {
-        FOR( j, WIDTH ) {
-            agent_map_[ i ][ j ].clear();
-        }
+  FOR( i, WIDTH ) {
+    FOR( j, WIDTH ) {
+      agent_map_[ i ][ j ].clear();
     }
+  }
 }
 
 /*
@@ -122,20 +123,24 @@ void Landscape :: clearAgentMap() {
  *--------------------------------------------------------------------------------------
  */
 void Landscape :: registAgent( const int x, const int y, Agent &a ) {
-    agent_map_[ x ][ y ].push_back( &a );                            /* エージェントを登録 */
+  agent_map_[ x ][ y ].push_back( &a );                              /* エージェントを登録 */
 }
+
+/*
+ *--------------------------------------------------------------------------------------
+ *      Method:  Landscape :: removeAgent
+ * Description:  
+ *--------------------------------------------------------------------------------------
+ */
 void Landscape :: removeAgent( const int x, const int y, Agent &a ) {
-    // ITERATOR(Agent *) it_a = getAgentIteratorBeginAt( x, y );        /* 指定の場所の配列の先頭から */
-    // while( it_a != getAgentIteratorEndAt( x, y ) ) {                 /* 末尾まで */
-    EACH( it_a, getAgentList(x, y) ) {
-        if( *it_a == &a ) {                                          /* エージェントを探して */
-            agent_map_[x][y].erase( it_a );                          /* 削除する */
-            return;                                                  /* 終了 */
-        }
-        // it_a++;
+  EACH( it_a, getAgentList(x, y) ) {
+    if( *it_a == &a ) {                                              /* エージェントを探して */
+      agent_map_[x][y].erase( it_a );                                /* 削除する */
+      return;                                                        /* 終了 */
     }
-    assert( ! ">>> this agent is not here !" );                      /* 見つからなければエラー */
-    return;
+  }
+  assert( ! ">>> this agent is not here !" );                        /* 見つからなければエラー */
+  return;
 }
 
 /*
