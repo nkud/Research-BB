21eeb528adfa5138676a71d4f1a5c9383e304b7f
Naoki Ueda <orange.s067803@gmail.com>
2014-06-26 12:05:02 +0900

    Random修正

--- ABEM/include/Random.hpp
+++ ABEM/include/Random.hpp
@@ -15,37 +15,63 @@
 #define ___RANDOM
 
 #include <random>
+#include <string>
+
+/*
+ * =====================================================================================
+ *        Class:  __RandomInterface
+ *  Description:  
+ * =====================================================================================
+ */
+class __Random {
+  public:
+    static __Random& Instance();
+    virtual void setRandomStrategy();
+
+    virtual int random();
+    virtual int uniformInt( int min, int max );
+    virtual int uniformDouble( double min, double max );
+
+    virtual std::string getStrategyName();                           /* 戦略名を取得 */
+  private:
+    __Random() {}
+};
+
+class __RandomStrategy {
+  public:
+  private:
+};
 
 class Random {
-public:
-	static Random& Instance() {
-		static Random singleton;
-		return singleton;
-	}
+  public:
+    static Random& Instance() {
+      static Random singleton;
+      return singleton;
+    }
 
-	std::mt19937& getMT() {
-		if ( mt == NULL ) {
-			std::random_device rd;
-			mt = new std::mt19937(rd());
-		}
-		return *mt;
-	}
-	int randomInt() {
-		return (getMT())();
-	}
-	double randomDouble();
-	int uniformInt( int min, int max ) {
-		std::uniform_int_distribution<int> random(min,max);
-		return random(getMT());
-	}
-	double uniformDouble( double min, double max ) {
-		std::uniform_real_distribution<double> random(min,max);
-		return random(getMT());
-	}
-private:
-	Random() {}
+    std::mt19937& getMT() {
+      if ( mt == NULL ) {
+        std::random_device rd;
+        mt = new std::mt19937(rd());
+      }
+      return *mt;
+    }
+    int randomInt() {
+      return (getMT())();
+    }
+    double randomDouble();
+    int uniformInt( int min, int max ) {
+      std::uniform_int_distribution<int> random(min,max);
+      return random(getMT());
+    }
+    double uniformDouble( double min, double max ) {
+      std::uniform_real_distribution<double> random(min,max);
+      return random(getMT());
+    }
+  private:
+    Random() {}
 
-	std::mt19937 *mt;
+    std::mt19937 *mt;
 };
 
 #endif
