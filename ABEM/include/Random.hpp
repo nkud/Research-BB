30c2baf3ced27e9fc3fb96a99b3a103e9f44a134
Naoki Ueda <orange.s067803@gmail.com>
2014-06-28 01:04:31 +0900

    ---

--- ABEM/include/Random.hpp
+++ ABEM/include/Random.hpp
@@ -23,14 +23,14 @@
 class Random;
 class __RandomStrategy;
 class __MersenneTwister;
+
+
 /*
  * =====================================================================================
- *        Class:  __RandomInterface
- *  Description:  
+ *        Class:  __RandomStrategy
+ *  Description:  乱数生成器の戦略, 純粋仮想関数
  * =====================================================================================
  */
-
-
 class __RandomStrategy {
   public:
     // virtual double randomDouble();
@@ -40,25 +40,30 @@ class __RandomStrategy {
   private:
 };
 
-///
-/// メルセンヌ・ツイスタ
+
+/*
+ * =====================================================================================
+ *        Class:  __MersenneTwister
+ *  Description:  メルセンヌ・ツイスタ法を使用した戦略
+ * =====================================================================================
+ */
 class __MersenneTwister : public __RandomStrategy {
   public:
-    std::mt19937& getMT() {
+    std::mt19937& getMT() {                                          /* 乱数生成関数を作成 */
       if ( mt == NULL ) {
         std::random_device rd;
         mt = new std::mt19937(rd());
       }
       return *mt;
     }
-    int randomInt() { return (getMT())(); }
+    int randomInt() { return (getMT())(); }                          /* 乱数生成 */
 
     // double randomDouble();
-    int uniformInt( int min, int max ) {
+    int uniformInt( int min, int max ) {                             /* min~maxの乱数を生成 */
       std::uniform_int_distribution<int> _random(min,max);
       return _random(getMT());
     }
-    double uniformDouble( double min, double max ) {
+    double uniformDouble( double min, double max ) {                 /* min~maxの乱数を生成 */
       std::uniform_real_distribution<double> _random(min,max);
       return _random(getMT());
     }
@@ -66,20 +71,16 @@ class __MersenneTwister : public __RandomStrategy {
     std::mt19937 *mt;
 };
 
+
+/*
+ * =====================================================================================
+ *        Class:  Random
+ *  Description:  乱数生成クラス
+ * =====================================================================================
+ */
 class Random {
   public:
-    static Random& Instance() { 
-      static Random singleton;
-      if ( singleton.getRandomStrategy() == NULL ) {
-        singleton.setRandomStrategy( new __MersenneTwister );
-      }
-      return singleton;
-    }
-    void setRandomStrategy( __RandomStrategy *rs ) {
-      random_strategy_ = rs;
-    }
-
-    int randomInt() { return random_strategy_->randomInt(); }
+    int randomInt() { return random_strategy_->randomInt(); }        /* 乱数を生成 */
     // double randomDouble() { return random_strategy_->randomDouble(); }
     int uniformInt( int min, int max ) {
       return random_strategy_->uniformInt( min, max );
@@ -88,16 +89,17 @@ class Random {
       return random_strategy_->uniformDouble( min, max );
     }
 
-    bool probability( double prob );
-    int randArray( const int n );
-    int randBinary();
-    bool randBool();
-    int randSign();
-
-    std::string getStrategyName();                           /* 戦略名を取得 */
     const __RandomStrategy* getRandomStrategy() const { return random_strategy_; }
+    void setRandomStrategy( __RandomStrategy *rs ) { random_strategy_ = rs; }
+
+    static Random& Instance() { 
+      static Random singleton;
+      if ( singleton.getRandomStrategy() == NULL ) {
+        singleton.setRandomStrategy( new __MersenneTwister );        /* メルセンヌ・ツイスタを使用 */
+      }
+      return singleton;
+    }
   private:
-    // __Random();   
      Random() {}
     __RandomStrategy *random_strategy_;
 };
