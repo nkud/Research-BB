1ecdbf1b125a9981167531e903be9510f51eb36b
Naoki Ueda <orange.s067803@gmail.com>
2014-06-30 16:55:08 +0900

    ---

--- ABEM/ABEMConfig.py
+++ ABEM/ABEMConfig.py
@@ -5,11 +5,13 @@ class Frame(Tk.Frame):
 
     def __init__(self, master=None):
         Tk.Frame.__init__(self, master)
-        self.master.title('Pack Three Labels')
+        self.master.title('ABEM Configure')
 
         # First Label
-        la = Tk.Label(self, text='Hello everybody. How are you?',  bg='yellow', relief=Tk.RIDGE, bd=2)
-        la.pack()
+        term = Tk.Label(self, text='TERM',  bg='yellow', relief=Tk.RIDGE, bd=2)
+        term.pack()
+        term_sp = Tk.Spinbox(self, width=10, from_=0, to=1000000)
+        term_sp.pack()
 
         # Second Label
         lb = Tk.Label(self, text='Oh My God!', bg='red', relief=Tk.RIDGE, bd=2)
@@ -22,10 +24,7 @@ class Frame(Tk.Frame):
         sp = Tk.Spinbox(self, width=10, from_=0, to=29)
         sp.pack()
 
-##----------------
 if __name__ == '__main__':
     f = Frame()
     f.pack()
     f.mainloop()
-    print "hello"
-    n = raw_input()
