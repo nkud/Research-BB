29fae04df7eb219c7ae736b9d4fedda8ddf34913
Naoki Ueda <orange.s067803@gmail.com>
2014-07-08 15:25:29 +0900

    ---

--- ABEM/ABEMConfig.py
+++ ABEM/ABEMConfig.py
@@ -8,11 +8,11 @@ incubation = int( raw_input("Incubation Period = ") )
 symptomatic = int( raw_input("Symptomatic Period = ") )
 
 command_incubation = 'cat "%s" | \
-    sed -e "/INCUBATION/c const int V_INCUBATION_PERIOD = %d;" \
+    sed -e "/INCUBATION_PERIOD/c const int V_INCUBATION_PERIOD = %d;" \
     > %s' \
     % ( CONFIG_PATH, incubation, CONFIG_PATH )
 command_lethal = 'cat "%s" | \
-    sed -e "/LETHAL/c const int V_LETHAL_PERIOD = %d;" \
+    sed -e "/LETHAL_PERIOD/c const int V_LETHAL_PERIOD = %d;" \
     > %s' \
     % ( CONFIG_PATH, incubation+symptomatic, CONFIG_PATH )
 
