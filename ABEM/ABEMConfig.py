9281dc8405585a8207a994d4b37a2c98ea9481e2
Naoki Ueda <orange.s067803@gmail.com>
2014-07-08 15:55:43 +0900

    modified ABEMConfig

--- ABEM/ABEMConfig.py
+++ ABEM/ABEMConfig.py
@@ -4,17 +4,33 @@ import os
 
 CONFIG_PATH = 'include/Config.h'
 
-incubation = int( raw_input("Incubation Period = ") )
-symptomatic = int( raw_input("Symptomatic Period = ") )
+def configure(inc, sym):
+  command_incubation = 'cat "%s" | \
+      sed -e "/INCUBATION_PERIOD/c const int V_INCUBATION_PERIOD = %d;" \
+      > %s' \
+      % ( CONFIG_PATH, inc, CONFIG_PATH )
+  command_lethal = 'cat "%s" | \
+      sed -e "/LETHAL_PERIOD/c const int V_LETHAL_PERIOD = %d;" \
+      > %s' \
+      % ( CONFIG_PATH, inc+sym, CONFIG_PATH )
 
-command_incubation = 'cat "%s" | \
-    sed -e "/INCUBATION_PERIOD/c const int V_INCUBATION_PERIOD = %d;" \
-    > %s' \
-    % ( CONFIG_PATH, incubation, CONFIG_PATH )
-command_lethal = 'cat "%s" | \
-    sed -e "/LETHAL_PERIOD/c const int V_LETHAL_PERIOD = %d;" \
-    > %s' \
-    % ( CONFIG_PATH, incubation+symptomatic, CONFIG_PATH )
+  os.system( command_incubation );
+  os.system( command_lethal );
 
-os.system( command_incubation );
-os.system( command_lethal );
+def run(inc, sym):
+  command = 'make build run gene pack mail R="IN%dSY%d"' \
+      % ( int(inc), int(sym) )
+  os.system( command );
+
+
+def main():
+  inc_range = 1
+  sym_range = 20
+
+  for inc in range(inc_range):
+    for sym in range(sym_range):
+      configure(inc, sym)
+      run(inc, sym)
+
+if __name__ == "__main__":
+  main();
