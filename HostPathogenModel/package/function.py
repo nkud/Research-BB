ac8e077c8a39e1497c9d0f83ab619c2a36ba826b
Naoki Ueda <orange.s067803@gmail.com>
2014-08-09 16:59:06 +0900

    random_chooseを追加: function.py

--- HostPathogenModel/package/function.py
+++ HostPathogenModel/package/function.py
@@ -24,16 +24,35 @@ def random_int(minimum, maximum):
   """
   return random.randint(minimum, maximum)
 
+def random_choose(alist):
+  """ 確率選択
+  Args:
+    alist: 選択されるリスト
+  Returns:
+    リストの中のどれか１つ
+  """
+  n = len(alist)
+  return alist[ random_int(0,n-1) ]
+
 ## 関数テスト
 class TestRandom(unittest.TestCase):
   def test_probability(self):
     for i in range(1000):
       self.assertTrue(probability(100))
       self.assertFalse(probability(0))
+
   def test_random_int(self):
     for i in range(1000):
       self.assertTrue(random_int(0,1) in [0,1])
       self.assertTrue(random_int(0,99) in range(100))
+
+  def test_choose(self):
+    l = []
+    for i in range(1000):
+      l.append(i)
+    for i in range(2000):
+      n = random_choose(l)
+      self.assertTrue( n>=0 and n<=999 )
 
 if __name__ == '__main__':
   unittest.main()
