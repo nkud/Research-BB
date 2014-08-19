e2e6a0635eef732da8828038b489c31076564fb8
Naoki Ueda <orange.s067803@gmail.com>
2014-08-19 16:38:24 +0900

    VirusEvolutionaryModelを初期化

---
+++ VirusEvolutionaryModel/include/Gene.hpp
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Gene.hpp
+ *
+ *    Description:  
+ *
+ *         Author:  Naoki Ueda
+ *   Organization:  OPU
+ *
+ * =====================================================================================
+ */
+
+#ifndef ___GENE_HPP
+#define ___GENE_HPP
+
+class Gene {
+  public:
+    int x;
+  private:
+};
+
+#endif
---
+++ VirusEvolutionaryModel/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *         Author:  Naoki Ueda
+ *   Organization:  OPU
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+#include "Gene.hpp"
+#include "Config.hpp"
+
+int main() {
+  std::cout << "Virus Evolutionary Model" << std::endl;
+  return 0;
+}
