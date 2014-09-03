18ec2052096668f01fa9f1ec466cd2ece25e3797
Naoki Ueda <su104003@gmail.com>
2014-09-03 09:49:36 +0900

    Configureの出力書式修正

--- VirusEvolutionaryModel/include/Config.hpp
+++ VirusEvolutionaryModel/include/Config.hpp
@@ -2,19 +2,19 @@
 #ifndef ___CONFIG_HPP
 #define ___CONFIG_HPP
 
-#define TERM 1000
-#define HUMAN_SIZE 1
-#define HUMAN_LAND_HEIGHT 10
-#define TCELL_LEN 100
-#define IMMUNE_INTERVAL 1
-#define TCELL_MINIMUM_SIZE 100
-#define CELL_LAND_WIDTH 40
-#define CELL_MAX_VIRUS_CAN_HAVE 10
-#define HUMAN_INTERVAL 1
-#define CELL_LAND_HEIGHT 40
-#define HUMAN_LAND_WIDTH 10
-#define V_INF_RATE 5
-#define V_TAG 10
-#define TCELL_LIFESPAN 10
+#define TERM                           1000      	///< 最大実行期間
+#define HUMAN_SIZE                     1         	///< 初期ヒト数
+#define HUMAN_LAND_HEIGHT              10        	///< ヒト土地タテ
+#define TCELL_LEN                      100       	///< T細胞遺伝子長
+#define IMMUNE_INTERVAL                1         	///< 免疫実行間隔
+#define TCELL_MINIMUM_SIZE             100       	///< 最小T細胞数
+#define CELL_LAND_WIDTH                40        	///< 細胞土地ヨコ
+#define CELL_MAX_VIRUS_CAN_HAVE        10        	///< 最大保持ウイルス数
+#define HUMAN_INTERVAL                 1         	///< ヒト実行間隔
+#define CELL_LAND_HEIGHT               40        	///< 細胞土地タテ
+#define HUMAN_LAND_WIDTH               10        	///< ヒト土地ヨコ
+#define V_INF_RATE                     5         	///< 感染率
+#define V_TAG                          10        	///< ウイルス遺伝子長
+#define TCELL_LIFESPAN                 10        	///< T細胞寿命
 
 #endif
