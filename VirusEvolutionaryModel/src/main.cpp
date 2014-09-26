56d6e10d33571902505456f05baa1f6628ad61e0
Naoki Ueda <su104003@gmail.com>
2014-09-26 12:44:15 +0900

    T細胞のメモリーを修正

--- VirusEvolutionaryModel/src/main.cpp
+++ VirusEvolutionaryModel/src/main.cpp
@@ -182,11 +182,11 @@ void run_host_pathogen_model( Human& human )
           if( probability( 100 * cell.calcDensityOfVirusSize() ) )
           {                                      // 細胞内のウイルス密度に比例して
             cell.clearInfectedViruses();         // ウイルスを除去して
-            Tcell& tcell = tcell.clone();
-            if( probability( TCELL_MEMORY_RATE ) ) {
-              tcell.becomeMemoryTcell();
+            Tcell& newtcell = tcell.clone();     // クローンを作成し
+            if( probability( TCELL_MEMORY_RATE ) ) {  // 記憶率の確率で
+              newtcell.becomeMemoryTcell();           // そのクローンをメモリーT細胞にする
             }
-            new_tcell.push_back( &( tcell.clone() ) ); // T細胞を増やす
+            new_tcell.push_back( &newtcell ); // T細胞を増やす
             break;
           }
         }
