edababecf03a55aa0390f61818fe50b1da5c7bd5
Naoki Ueda <su104003@gmail.com>
2015-01-21 02:12:47 +0900

    ---

--- VirusEvolutionaryModel/src/main.cpp
+++ VirusEvolutionaryModel/src/main.cpp
@@ -50,11 +50,14 @@ main()
   ECHO("ヒト初期化");
   VECTOR(Human *) humans;
   Human *h;
+  // ヒトを初期設定の数だけ新しく作成する。
+  // 各ヒトに対しては、
+  // 細胞土地を作成する。
   FOR( i, HUMAN_SIZE ) {                          // 初期設定の数だけ
     h = new Human( TCELL_LEN,
-      TCELL_MINIMUM_SIZE,
-      new CellLand(CELL_LAND_WIDTH, CELL_LAND_HEIGHT, "0000000000")
-      ); // 新しくヒトを初期化
+                   TCELL_MINIMUM_SIZE,
+                   new CellLand(CELL_LAND_WIDTH, CELL_LAND_HEIGHT, "0000000000")
+                   );
     h->randomLocate(*humanLand);              // ランダムに土地に配置して
     humans.push_back( h );                    // 配列に追加していく
   }
